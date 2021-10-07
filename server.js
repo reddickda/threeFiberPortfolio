68245d96df9a6a187589187d6cc2f6f4dafbc0ba
David <reddickd@terpmail.umd.edu>
2021-10-07 02:16:15 -0400

    more express attempts

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
-
+const path = require("path");
+const app = express();
 const PORT = process.env.PORT || 3001;
 
-const app = express();
+app.use(express.static(path.join(__dirname, "build")));
+
+// This route serves the React app
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "build", "index.html")));
 
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
