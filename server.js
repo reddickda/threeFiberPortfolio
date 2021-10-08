f9af73e1ace427414e76406a37a56265b3621632
David <reddickd@terpmail.umd.edu>
2021-10-08 14:41:20 -0400

    trying to add cors policy

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const cors = require('cors');
+
 const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3001;
 const { Client } = require('pg');
-
+app.use(cors({
+    origin: ['https://simmer.io', 'https://david-portfolio-site.herokuapp.com/', 'https://www.davidthehobbyist.com/', 'http://www.davidthehobbyist.com/']
+}));
 app.use(express.static(path.join(__dirname, "build")));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
